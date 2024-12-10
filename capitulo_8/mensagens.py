ead05619d482c89cb30aefbe8a8f048ac484f57a
nelcostaa <nelcostaa@hotmail.com>
2024-12-10 14:11:35 -0300

    continue exercices on functions

---
+++ capitulo_8/mensagens.py
@@ -0,0 +1,8 @@
+def mostrar_mensagens(mensagens):
+    """Imprime as mensagems de uma lista"""
+    for mensagem in mensagens:
+        print(mensagem)
+
+
+mensagens = ["ola mundo", "tchau mundo", "python e bom"]
+mostrar_mensagens(mensagens)
