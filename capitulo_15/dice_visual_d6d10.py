e7c213fbfe1ffd8b2117f0fadd1dc3b4f0a62880
nelcostaa <nelcostaa@hotmail.com>
2025-01-08 20:32:39 -0300

    add last codes from chapter_15

---
+++ capitulo_15/dice_visual_d6d10.py
@@ -0,0 +1,32 @@
+import plotly.express as px
+
+from die import Die
+
+# Cria um D6
+die_1 = Die()
+die_2 = Die(10)
+
+# Realiza alguns testes e armazena os resultados em uma lista
+results = []
+for roll_num in range(50_000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analisa os resultados
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualiza os resultados
+title = "Resultados de Rolar um D6 e D10 50.000 vezes"
+labels = {"x": "Resultado", "y": "Frequencia do Resultado"}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Personaliza ainda mais o grafico
+fig.update_layout(xaxis_dtick=1)
+
+# fig.show()
+fig.write_html("capitulo_15/dice_visual_d6d10.html")
---
+++ capitulo_15/die.py
@@ -0,0 +1,13 @@
+from random import randint
+
+
+class Die:
+    """Classe que representa um unico dado"""
+
+    def __init__(self, num_sides=6):
+        """Faz a suposicao de que um dado tem seis lados"""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """Retorna um valor aleatorio entre 1 e o numero de lados"""
+        return randint(1, self.num_sides)
